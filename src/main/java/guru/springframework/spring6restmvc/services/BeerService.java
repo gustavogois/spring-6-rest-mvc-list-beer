f32adfc0c5f527acb08f10f72dae94f41d6d45d0
Gois-Prestataire GUSTAVO (KLX) [28212e10-35f0-4a99-af38-4d6fd950fda7] <gustavo.gois-prestataire@ca-ps.com>
2023-02-22 14:05:05 +0000

    Initial commit

---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Beer;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+public interface BeerService {
+
+    List<Beer> listBeers();
+
+    Beer getBeerById(UUID id);
+}
